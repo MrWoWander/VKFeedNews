7898f36d58f419aa4718521f54db0461f2d2d714
Mikhail <wowivan2011@yandex.ru>
2021-10-13 16:12:18 +0300

    get profile and group

--- VKFeedNews/Newsfeed/NewsfeedPresenter.swift
+++ VKFeedNews/Newsfeed/NewsfeedPresenter.swift
@@ -9,33 +9,57 @@
 import UIKit
 
 protocol NewsfeedPresentationLogic {
-  func presentData(response: Newsfeed.Model.Response.ResponseType)
+    func presentData(response: Newsfeed.Model.Response.ResponseType)
 }
 
 class NewsfeedPresenter: NewsfeedPresentationLogic {
-  weak var viewController: NewsfeedDisplayLogic?
-  
-  func presentData(response: Newsfeed.Model.Response.ResponseType) {
-      switch response {
-      case .presentNewsfeed(let feed):
-          
-          let cells = feed.items.map { cellViewModel(from: $0) }
-          
-          let feedViewModel = FeedViewModel.init(cells: cells)
-          viewController?.displayData(viewModel: .displayNewsfeed(feedViewModel: feedViewModel))
-      }
-  }
-  
-    private func cellViewModel(from feedItem: FeedItem) -> FeedViewModel.Cell {
+    weak var viewController: NewsfeedDisplayLogic?
+    
+    let dateFormatter: DateFormatter = {
+        let dt = DateFormatter()
+        dt.locale = Locale(identifier: "ru_RU")
+        dt.dateFormat = "d MMM 'в' HH:mm"
         
-        return FeedViewModel.Cell.init(iconURLString: "",
-                                       name: "future name",
-                                       date: "future date",
+        return dt
+    }()
+    
+    
+    func presentData(response: Newsfeed.Model.Response.ResponseType) {
+        switch response {
+        case .presentNewsfeed(let feed):
+            
+            let cells = feed.items.map { cellViewModel(from: $0, profiles: feed.profiles, groups: feed.groups) }
+            
+            let feedViewModel = FeedViewModel.init(cells: cells)
+            viewController?.displayData(viewModel: .displayNewsfeed(feedViewModel: feedViewModel))
+        }
+    }
+    
+    private func cellViewModel(from feedItem: FeedItem, profiles: [Profile], groups: [Group]) -> FeedViewModel.Cell {
+        
+        let profile = profile(sourceId: feedItem.sourceId, profiles: profiles, groups: groups)
+        
+        let date = Date(timeIntervalSince1970: feedItem.date)
+        let dateTitle = dateFormatter.string(from: date)
+        
+        return FeedViewModel.Cell.init(iconURLString: profile.photo,
+                                       name: profile.name,
+                                       date: dateTitle,
                                        text: feedItem.text,
                                        comments: String(feedItem.comments?.count ?? 0),
                                        likes: String(feedItem.likes?.count ?? 0),
                                        shares: String(feedItem.reposts?.count ?? 0),
                                        views: String(feedItem.views?.count ?? 0))
+    }
+    
+    private func profile(sourceId: Int, profiles: [Profile], groups: [Group]) -> ProfileRepresentable {
+        
+        let profilesOrGroups: [ProfileRepresentable] = sourceId > 0 ? profiles : groups
+        
+        let normalSourceId = sourceId >= 0 ? sourceId : -sourceId
+        
+        return profilesOrGroups.first(where: { $0.id == normalSourceId })!
+        
     }
     
 }
